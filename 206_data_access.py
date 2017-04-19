f120d4b30bb8eddfdc9b378483878e14a7c6069b
Shuyuan Xiao <shuyuanx@umich.edu>
2017-04-18 23:29:12 -0400

    Added table tweets

--- 206_data_access.py
+++ 206_data_access.py
@@ -86,15 +86,18 @@ class Movie(object):
 		actors_list = self.actors.split(",")
 		return actors_list
 
+	def get_director(self):
+		return self.director
+
 	def __str__(self):
 		return "The movie named {} is released in {}, directed by {}. Actors include {}, and the writer is {}. The language of the movie is {}. It is rated {} by IMDB.".format(self.title, self.director, self.actors, self.writer, self.language, self.rating)
 
-#this is what the result for get movie data should look like. Only showing it here for reference. And it's just used for testing for now. It will be changed it later, and won't be hardcoded in function or classes.
+#this is what the result for get movie data should look like. Only showing it here for reference. Won't be hardcoded in function or classes.
 movie_ghost = {"Title":"Ghost","Year":"1990","Rated":"PG-13","Released":"13 Jul 1990","Runtime":"127 min","Genre":"Drama, Fantasy, Romance","Director":"Jerry Zucker","Writer":"Bruce Joel Rubin","Actors":"Patrick Swayze, Demi Moore, Whoopi Goldberg, Tony Goldwyn","Plot":"After a young man is murdered, his spirit stays behind to warn his lover of impending danger, with the help of a reluctant psychic.","Language":"English","Country":"USA","Awards":"Won 2 Oscars. Another 16 wins & 22 nominations.","Poster":"https://images-na.ssl-images-amazon.com/images/M/MV5BMTU0NzQzODUzNl5BMl5BanBnXkFtZTgwMjc5NTYxMTE@._V1_SX300.jpg","Ratings":[{"Source":"Internet Movie Database","Value":"7.0/10"},{"Source":"Rotten Tomatoes","Value":"74%"},{"Source":"Metacritic","Value":"52/100"}],"Metascore":"52","imdbRating":"7.0","imdbVotes":"160,136","imdbID":"tt0099653","Type":"movie","DVD":"24 Apr 2001","BoxOffice":"N/A","Production":"Paramount Pictures","Website":"N/A","Response":"True"}
 
 
 #class Tweet(object):
-
+	#define later: needs to represent twitter info for a user. I would search the directors for the movies, so this class will represent the tweets written by directors. 
 
 
 #write the main function: write the class instantiation and function calls in main. The process of actually obtaining and parsing data should go in main here. 
@@ -114,7 +117,7 @@ twitter_result1 = get_data_from_twitter(movie1.director)
 print(type(twitter_result1))
 print(twitter_result1)
 
-#set up a table and input the data into the table
+#set up a table Movies for movies and input the data into the table
 # Make a connection to a new database tweets.db, and create a variable to hold the database cursor.
 conn = sqlite3.connect('movies_twitters.db') 
 cur = conn.cursor()
@@ -153,11 +156,45 @@ print(movie_data)
 
 cur.execute(statement, movie_data)
 
+# Set up a table Tweets for tweets
+# Write code to drop the Tweets table if it exists, and create the table (so you can run the program over and over), with the correct (4) column names and appropriate types for each.
+# HINT: Remember that the time_posted column should be the TIMESTAMP data type!
+cur.execute('DROP TABLE IF EXISTS Tweets')
 
+table_spec = 'CREATE TABLE IF NOT EXISTS '
+table_spec += 'Tweets (tweet_id INTEGER, ' 
+table_spec += 'author TEXT, ' 
+table_spec += 'time_posted TIMESTAMP, ' 
+table_spec += 'tweet_text TEXT, ' 
+table_spec += 'retweets INTEGER)' 
+cur.execute(table_spec)
+
+statement = 'INSERT INTO Tweets VALUES (?, ?, ?, ?, ?)'
+
+#get twitters later
+twitters = []
+for tweet in twitters:
+	tweet_info = []
+	tweet_info.append(tweet["id"])
+	tweet_info.append(tweet["user"]["screen_name"])
+	tweet_info.append(tweet["created_at"])
+	tweet_info.append(tweet["text"])
+	tweet_info.append(tweet["retweet_count"])
+	cur.execute(statement, tweet_info)
+
+# Use the database connection to commit the changes to the database
+conn.commit()
+
+
+#make three more calls to the get movie data function
 #create three instances of class Movie using the data obtained above
+#add the data to the table Movies
 
+#make three more calls to the get data from twitter function
 #instantiate the class Tweet to get data from twitter here
+#add the data to the table Tweets
 
+#invoke the main function
 #if __name__ == "__main__":
 	#main()
 
@@ -167,19 +204,19 @@ cur.execute(statement, movie_data)
 # Modify these tests and add more teste later. Need to test the functions and the classes as well.
 class test_class_Movie(unittest.TestCase):
 	def test_class_ctor(self):
-		movie1 = Movie({movie_ghost})
+		movie1 = Movie({movie1_data})
 		self.assertEqual(type(movie1.director, type("abs")))
 
 	def test_get_writer(self):
-		movie1 = Movie({movie_ghost})
+		movie1 = Movie({movie1_data})
 		self.assertEqual(type(movie1.get_list_of_actors(), type(["a","b"])))
 
 	def test_get_actors(self):
-		movie1 = Movie({movie_ghost})
+		movie1 = Movie({movie1_data})
 		self.assertEqual(type(movie1.actors), type("actors"))
 
 	def test_process_data(self):
-		movie1 = Movie({movie_ghost})
+		movie1 = Movie({movie1_data})
 		self.assertEqual(type(movie1.released), type("actors"))
 
 class class_Twitter(unittest.TestCase):
